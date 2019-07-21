dfc103e6973ebdce87d06ef378daacc939a44cb2
HenriqueSabino <h.sabinocosta@gmail.com>
2019-07-21 16:34:05 -0300

    Adding the processing library

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,18 +1,23 @@
 package application;
 
+import application.screens.Screen;
 import io.github.henriquesabino.training.TrainingSet;
+import processing.core.PApplet;
+import processing.core.PVector;
 import services.MnistReader;
 
 import java.io.File;
 import java.io.IOException;
 
-public class Main {
+public class Main extends PApplet {
     
     private static TrainingSet trainingSet = null;
+    private Screen leftScreen, rightScreen, drawingScreen, textOut;
     
     public static void main(String[] args) {
         
         loadTrainingSet();
+        PApplet.main("application.Main");
     }
     
     private static void loadTrainingSet() {
@@ -40,5 +45,16 @@ public class Main {
     //temporary method, it will be replaced with a Processing sketch
     private static void loadTestingSet() {
     
+    }
+    
+    @Override
+    public void settings() {
+        size(560, 560);
+        
+        leftScreen = new Screen(new PVector(0, 0), new PVector(width / 2, height));
+        rightScreen = new Screen(new PVector(width / 2 + 1, 0), new PVector(width, height));
+        
+        drawingScreen = new Screen(new PVector(0, 0), new PVector(28, 28), leftScreen);
+        textOut = new Screen(new PVector(0, 0), new PVector(28, 28), rightScreen);
     }
 }
---
+++ src/application/screens/Screen.java
@@ -0,0 +1,90 @@
+package application.screens;
+
+import processing.core.PVector;
+
+public final class Screen {
+    
+    private Screen parent;
+    private PVector[] points = new PVector[2];
+    private float width;
+    private float height;
+    private float pixelSize;
+    
+    public Screen(float width, float height) {
+        
+        this.width = width;
+        this.height = height;
+        
+        points[0] = new PVector();
+        points[1] = new PVector(width, height);
+        
+        parent = null;
+        pixelSize = 1;
+    }
+    
+    public Screen(PVector v1, PVector v2) {
+        
+        setPoints(new PVector[]{v1, v2});
+        
+        parent = null;
+        pixelSize = 1;
+    }
+    
+    public Screen(PVector v1, PVector v2, Screen parent) {
+        points[0] = v1;
+        points[1] = v2;
+        
+        width = Math.abs(v2.x - v1.x);
+        height = Math.abs(v2.y - v1.y);
+        
+        //note: only support square planes for now
+        this.parent = parent;
+        this.pixelSize = parent.getWidth() / getWidth();
+    }
+    
+    public PVector[] getPoints() {
+        return points;
+    }
+    
+    public void setPoints(PVector[] points) {
+        this.points = points;
+        
+        width = Math.abs(points[1].x - points[0].x);
+        height = Math.abs(points[1].y - points[0].y);
+    }
+    
+    public float getX(float x) {
+        if (parent != null)
+            return parent.getX(x * pixelSize + points[0].x);
+        else
+            return x * pixelSize + points[0].x;
+    }
+    
+    public float getY(float y) {
+        if (parent != null)
+            return parent.getY(y * pixelSize + points[0].y);
+        else
+            return y * pixelSize + points[0].y;
+    }
+    
+    public float getWidth() {
+        return width;
+    }
+    
+    public float getHeight() {
+        return height;
+    }
+    
+    public Screen getParent() {
+        return parent;
+    }
+    
+    public void setParent(Screen parent) {
+        this.parent = parent;
+        this.pixelSize = parent.getWidth() / getWidth();
+    }
+    
+    public float getPixelSize() {
+        return pixelSize;
+    }
+}
